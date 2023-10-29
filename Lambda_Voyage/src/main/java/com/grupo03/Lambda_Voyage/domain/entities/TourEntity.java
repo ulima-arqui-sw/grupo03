643fbbb968d72798ad1fa3143ccab664acf2deb4
Steep Salvador <stevesalvadorman@gmail.com>
2023-10-29 13:54:14 -0500

    modelado-casi-completo/Tengo sueño

---
+++ Lambda_Voyage/src/main/java/com/grupo03/Lambda_Voyage/domain/entities/TourEntity.java
@@ -0,0 +1,41 @@
+package com.grupo03.Lambda_Voyage.domain.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity(name = "tour")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class TourEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @OneToMany(
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER,
+            orphanRemoval = true,
+            mappedBy = "tour"
+    )
+    private Set<ReservationEntity> reservations;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @OneToMany(
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER,
+            orphanRemoval = true,
+            mappedBy = "tour"
+    )
+    private Set<TicketEntity> tickets;
+
+    @ManyToOne
+    @JoinColumn(name = "id_customer")
+    private CustomerEntity customer;
+}
