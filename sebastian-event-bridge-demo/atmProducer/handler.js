062c16adc0d8c3cbd0342f912d4a7964deef2ba9
Sebastián Chavarry <69838707+sebastianperudev2001@users.noreply.github.com>
2023-12-03 15:18:30 -0500

    Cambios
    xd

---
+++ sebastian-event-bridge-demo/atmProducer/handler.js
@@ -0,0 +1,15 @@
+const AWS = require('aws-sdk')
+AWS.config.region = process.env.AWS_REGION || 'us-east-1'
+const eventbridge = new AWS.EventBridge()
+
+exports.lambdaHandler = async (event, context) => {
+
+  const { params } = require('./events.js')
+
+  console.log('--- Params ---')
+  console.log(params)
+  const result = await eventbridge.putEvents(params).promise()
+
+  console.log('--- Response ---')
+  console.log(result)  
+}
